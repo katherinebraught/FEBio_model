5467003d9fa3fc6740f0e2ca300b239e41cf26ba
katherinebraught <katherinebraught@gmail.com>
2020-06-04 11:41:06 -0500

    fix error with input file reuse

--- pipelineFun.sh
+++ pipelineFun.sh
@@ -10,16 +10,17 @@ febioOut=$2
 timepoints_data=timepoints_data
 lifted_input=lift_$inputfile
 modified_strain_values=mod_$febioOut
-temp_input=new_model_$inputfile
+temp_input=temp_$inputfile
+final_model=new_model_$inputfile
 
 #first step: run on inputfile to get a list of timepoints
 echo creating lifted file
 FEBio3 -i $inputfile > febio_output_t0
 python find_timepoints.py $febioOut $timepoints_data
 
-
+cp $inputfile $temp_input
 #for loop goes here
-for timepoint in 5 10 15
+for timepoint in 3 5 7 9 11
 do
 #generate a model with lift from original inputfile
 python create_mod_model.py $inputfile $lifted_input $timepoints_data $timepoint
@@ -27,8 +28,9 @@ python create_mod_model.py $inputfile $lifted_input $timepoints_data $timepoint
 FEBio3 -i $lifted_input > febio_lifted_output_t$timepoint
 
 echo removing time point $timepoint
-python remove_timepointdata.py $febioOut $modified_strain_values strain_values_output threashold_values_output $inputfile $temp_input $timepoint
+python remove_timepointdata.py $febioOut $modified_strain_values strain_values_output threashold_values_output $temp_input $final_model $timepoint
 FEBio3 -i $temp_input > febio_output_t$timepoint
+cp $final_model $temp_input
 done
 
 
